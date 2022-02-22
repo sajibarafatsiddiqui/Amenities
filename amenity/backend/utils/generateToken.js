c80d3bdb8f4feb16cfb4ab8fbed9c21cf8b563a3
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2022-02-22 07:34:15 +0600

    third

--- amenity/backend/utils/generateToken.js
+++
@@ -1,6 +0,0 @@
-import jsonwebtoken from 'jsonwebtoken'
-
-const generateToken = (id) =>
-  jsonwebtoken.sign({ id }, process.env.JWS_SECRET_KEY, { expiresIn: '30d' })
-
-export default generateToken
